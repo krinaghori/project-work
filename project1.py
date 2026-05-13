6708f02373bab63f425ea908b14d72859000d157
krinaghori <krinaghori2@gmail.com>
2026-05-13 22:50:28 +0530

    1 project

---
+++ project1.py
@@ -0,0 +1,30 @@
+print("WELCOME TO THE PERSONAL DATA INTAKE FORM !")
+print()
+
+input("please enter your name:-")
+print("type:",type("please enter your name:-"))
+print("memory address:",id("please enter your name:-"))
+print()
+
+A = int(input("please enter your age:-"))
+print("type:",type(A))
+print("memory address:",id(A))
+print()
+
+B = float(input("please enter your height in meters:-"))
+print("type:",type(B))
+print("memory address:",id(B))
+print()
+
+C = int(input("please enter your favourite number:-"))
+print("type:",type(C))
+print("memory address:",id(C))
+print()
+
+print("your birth year is approximately:",+2026-A)
+print()
+
+print("THANK YOU FOR USING THE PERSONAL DATA INTAKE FORM.")
+print("GOODBYE !")
+
+
